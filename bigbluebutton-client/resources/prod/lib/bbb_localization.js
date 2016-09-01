70e33fa034825d8ca071b9f7279eb4ec1963240b
Daniel Nilsson <danielenilsson@gmail.com>
2016-09-01 10:38:00 +0200

    Updates the locale file for swedish

--- bigbluebutton-client/resources/prod/lib/bbb_localization.js
+++ bigbluebutton-client/resources/prod/lib/bbb_localization.js
@@ -1,27 +1,27 @@
 // Check locale
-var localeChain = "sv_SE";
+// var localeChain = "sv_SE";
 
-if (navigator.language)
-        localeChain = navigator.language;
-else if (navigator.browserLanguage)
-        localeChain = navigator.browserLanguage;
+// if (navigator.language)
+//         localeChain = navigator.language;
+// else if (navigator.browserLanguage)
+//         localeChain = navigator.browserLanguage;
 
-if(/^([a-z]+)-([a-z]+)$/i.test(localeChain)){
-        var matches = /^([a-z]+)-([a-z]+)$/i.exec(localeChain);
-        localeChain = matches[1].toLowerCase() + "_" + matches[2].toUpperCase();
-}
-else if(/^en$/i.test(localeChain)){
-        localeChain = "en_US";
-}
-else if(/^([a-z]+)$/i.test(localeChain)){
-        var matches = /^([a-z]+)$/i.exec(localeChain);
-        localeChain = matches[1].toLowerCase() + "_" + matches[1].toUpperCase();
-}
-else{
-        localeChain = "sv_SE";
-}
+// if(/^([a-z]+)-([a-z]+)$/i.test(localeChain)){
+//         var matches = /^([a-z]+)-([a-z]+)$/i.exec(localeChain);
+//         localeChain = matches[1].toLowerCase() + "_" + matches[2].toUpperCase();
+// }
+// else if(/^en$/i.test(localeChain)){
+//         localeChain = "en_US";
+// }
+// else if(/^([a-z]+)$/i.test(localeChain)){
+//         var matches = /^([a-z]+)$/i.exec(localeChain);
+//         localeChain = matches[1].toLowerCase() + "_" + matches[1].toUpperCase();
+// }
+// else{
+//         localeChain = "sv_SE";
+// }
 // Stritcly sets the language to swedish, remove this line to enable dynamic language changes. Based on the code above.
-localeChain = "sv_SE";
+var localeChain = "sv_SE";
 
 function getLanguage(){
         return localeChain;
